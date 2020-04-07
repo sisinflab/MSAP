fe082fb57ef17199946272de43b587672d4b22ff
MerraEng <merrafelice@gmail.com>
2020-04-07 16:54:29 +0200

    implemented bim and pgd

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ from util.read import read_config
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run train of the Recommender Model.")
-    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--dataset', nargs='?', default='movielens-1m', help='dataset path: movielens-1m, gowalla, lastfm, yelp')
     parser.add_argument('--rec', nargs='?', default="bprmf", help="bprmf, amr")
     parser.add_argument('--batch_size', type=int, default=512, help='batch_size')
